f680b0529b245eac48554c49f35a6ff19de9e704
laxmishukla25 <laxmi.shukla@whitestones.co.in>
2026-05-15 12:40:49 +0530

    Config_file

---
+++ src/main/java/whitestone/trainee_management/config/WebCorsConfig.java
@@ -0,0 +1,24 @@
+package whitestone.trainee_management.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebCorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://192.168.0.21:9779", "https://application.rocket.new")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
